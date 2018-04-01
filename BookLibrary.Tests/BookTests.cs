31f915397ee3ec996341487b848db09d0974f4c6
Vasya <inqire@gmail.com>
2018-04-01 14:21:00 +0300

    Added missing comments.

---
+++ BookLibrary.Tests/BookTests.cs
@@ -0,0 +1,13 @@
+﻿namespace BookLibrary.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BookTests
+    {
+
+        public void TestMethod1()
+        {
+        }
+    }
+}
